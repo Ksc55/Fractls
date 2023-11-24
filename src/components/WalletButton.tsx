107c78284641a960d549a40314531b932055f333
king lee <c-king.lee@hulu.com>
2023-11-23 22:48:26 -0600

    connect wallet button working

--- src/components/WalletButton.tsx
+++ src/components/WalletButton.tsx
@@ -1,7 +1,17 @@
+'use client'
+import { useAccount, useConnect, useDisconnect } from 'wagmi'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+
 export default function WalletButton() {
-    return (
-        <button className={'font-bold border-2 border-customGreen-50 rounded-full px-4 h-10 text-base'}>
-            CONNECT WALLET
-        </button>
-    )
+        const { address, isConnected } = useAccount()
+    console.log(address)
+        const { connect } = useConnect({
+            connector: new InjectedConnector(),
+        })
+        const { disconnect } = useDisconnect()
+        const styleButton = 'font-bold border-2 border-customGreen-50 rounded-full px-4 h-10 text-base'
+        if (isConnected) {
+            return (<button className={styleButton} onClick={() => disconnect()}>DISCONNECT</button>)
+        }
+        return (<button className={styleButton} onClick={() => connect()}>CONNECT WALLET</button>)
 }
