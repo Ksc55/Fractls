65358b68db6ede7100c227197b9b46da6981d1e7
king lee <c-king.lee@hulu.com>
2023-11-26 18:54:38 -0600

    fix: hidration error

--- src/components/WalletButton.tsx
+++ src/components/WalletButton.tsx
@@ -1,15 +1,17 @@
 'use client'
 import { useAccount, useConnect, useDisconnect } from 'wagmi'
 import { InjectedConnector } from 'wagmi/connectors/injected'
+import {useIsMounted} from "@/components/useIsMounted";
 
 export default function WalletButton() {
         const { address, isConnected } = useAccount()
+        const mounted  = useIsMounted()
         const { connect } = useConnect({
             connector: new InjectedConnector(),
         })
         const { disconnect } = useDisconnect()
         const styleButton = 'font-bold border-2 border-customGreen-50 rounded-full px-4 h-10 text-base'
-        if (isConnected) {
+        if (isConnected && mounted) {
             return (<button className={styleButton} onClick={() => disconnect()}>DISCONNECT</button>)
         }
         return (<button className={styleButton} onClick={() => connect()}>CONNECT WALLET</button>)
