82f8a07bc8e4bf12aa018c3285ca25f98a7974c4
king lee <c-king.lee@hulu.com>
2023-11-27 18:57:52 -0600

    fix: removing errors from other unused wallets

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,10 +4,7 @@ import { WagmiConfig, createConfig, configureChains, mainnet } from 'wagmi'
 import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from 'wagmi/providers/public'
 
-import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
-import { InjectedConnector } from 'wagmi/connectors/injected'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 import {polygonMumbai} from "viem/chains";
 
 
@@ -19,29 +16,10 @@ const { chains, publicClient, webSocketPublicClient } = configureChains(
 const config = createConfig({
     autoConnect: true,
     connectors: [
-        new MetaMaskConnector({ chains }),
-        new CoinbaseWalletConnector({
-            chains,
-            options: {
-                appName: 'wagmi',
-            },
-        }),
-        new WalletConnectConnector({
-            chains,
-            options: {
-                projectId: '...',
-            },
-        }),
-        new InjectedConnector({
-            chains,
-            options: {
-                name: 'Injected',
-                shimDisconnect: true,
-            },
-        }),
+        new MetaMaskConnector({ chains })
     ],
     publicClient,
-    webSocketPublicClient,
+    webSocketPublicClient
 })
 export function Providers({ children }) {
     return (
