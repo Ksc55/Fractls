dac5f759fd2ec5e9022ee37723d4466e592aba84
king lee <c-king.lee@hulu.com>
2023-11-27 20:06:52 -0600

    feature: original nft

---
+++ src/app/nft/[id]/fraction/page.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+
+export default function Page() {
+
+    return (<>
+        <div className="flex flex-col  h-screen">
+            <section className={'w-100  bg-[#d9d9d9]  h-3/6 flex justify-center items center'}>
+                <Image src={'/image-19.png'}  width={600} height={600} className={'p-4'} />
+            </section>
+            <section className={'flex  w-full mt-6'}>
+                <div className={'w-3/6'}>
+                    <div className={'grid grid-cols-1 gap-1'}>
+                        <span className="text-black/[.70] font-['Roboto'] text-[.9375rem] leading-[normal]">Owned By</span>
+                        <span className="text-black font-['Roboto'] text-2xl leading-[normal]">@Collector</span>
+                    </div>
+                </div>
+                <div>
+                    <div className=" w-[475px] h-16 rounded-[0.875rem] border border-black bg-white mt-4 flex justify-between items-center px-2">
+                        <div className={'flex flex-col'}>
+                            <div className="text-black/[.70] font-['Roboto'] text-[.9375rem] leading-[normal]">Last Sold</div>
+                            <div className="text-black font-['Roboto'] text-2xl leading-[normal]">0.55 ETH</div>
+                        </div>
+                        <button className="flex-shrink-0 w-[7.1875rem] h-10 rounded-full bg-customGreen-50">
+                            Buy
+                        </button>
+                    </div>
+                </div>
+            </section>
+            <div className={'flex justify-between items-center mt-20'}>
+                <Image src={'/back-arrow.svg'}  width={80} height={80} className={'p-4'} />
+                <div className="text-[#121212] text-center font-['Roboto'] text-[2rem] font-semibold leading-[150%]">Similar F-NFTs</div>
+                <Image src={'/next-arrow.svg'}  width={80} height={80} className={'p-4'} />
+            </div>
+            <div className={'flex justify-between items-center'}>
+                <Image src={'/image-19.png'}  width={250} height={250} />
+                <Image src={'/image-19.png'}  width={250} height={250} />
+                <Image src={'/image-19.png'}  width={250} height={250} />
+                <Image src={'/image-19.png'}  width={250} height={250} />
+                <Image src={'/image-19.png'}  width={250} height={250} />
+
+            </div>
+        </div>
+    </>)
+}
+
+
+
