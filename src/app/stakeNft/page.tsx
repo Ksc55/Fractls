84c7bda00a07c2af929c0a4aa760e03bf9aad9ef
Ferdinand Ogar <116808690+webinand@users.noreply.github.com>
2023-11-29 17:58:26 +0100

    stake nfts
    completed

---
+++ src/app/stakeNft/page.tsx
@@ -0,0 +1,287 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="relative bg-whitesmoke-200 w-full h-[1909px] overflow-hidden text-left text-sm text-darkslategray font-roboto">
+      <div className="absolute top-[249px] left-[calc(50%_-_557px)] rounded bg-gainsboro-100 w-[1120px] h-[421px] overflow-hidden text-base">
+        <div className="absolute top-[61px] left-[81px] rounded-t rounded-b-none bg-darkslategray w-[300px] h-[300px] overflow-hidden">
+          <img
+            className="absolute top-[0px] left-[0px] w-[300px] h-[300px] object-cover"
+            alt=""
+            src="/image-19@2x.png"
+          />
+        </div>
+        <div className="absolute top-[373px] left-[81px] text-xs leading-[150%] flex items-center w-[59px] h-4">
+          Curated By
+        </div>
+        <div className="absolute top-[389px] left-[81px] leading-[150%] font-medium flex items-center w-[70px] h-4">
+          CoolDeep
+        </div>
+        <div className="absolute bottom-[110px] left-[calc(50%_-_158px)] w-[663px] h-[199px] text-gray">
+          <div className="absolute bottom-[0px] left-[calc(50%_-_114.5px)] rounded-31xl bg-lime w-[230px] h-11 overflow-hidden">
+            <div className="absolute top-[calc(50%_-_12px)] left-[calc(50%_-_37px)] leading-[150%]">
+              Stake NFT
+            </div>
+          </div>
+          <div className="absolute top-[0px] left-[0px] w-[663px] h-[131px] text-darkslategray">
+            <div className="absolute top-[5px] left-[0px] w-[180px] h-[51px]">
+              <div className="absolute top-[0px] left-[0px] leading-[120%] font-medium">
+                Total Value Locked (TVL)
+              </div>
+              <div className="absolute top-[27px] left-[47px] text-xl leading-[120%] font-medium text-gray">
+                $870,000
+              </div>
+            </div>
+            <div className="absolute top-[80px] left-[23px] w-[134px] h-[51px]">
+              <div className="absolute top-[0px] left-[0px] leading-[120%] font-medium">
+                Total Staked NFT’s
+              </div>
+              <div className="absolute top-[27px] left-[36px] text-xl leading-[120%] font-medium text-gray">
+                17,000
+              </div>
+            </div>
+            <div className="absolute top-[5px] left-[293px] w-[134px] h-[51px]">
+              <div className="absolute top-[0px] left-[0px] leading-[120%] font-medium">
+                Total Staked NFT’s
+              </div>
+              <div className="absolute top-[27px] left-[36px] text-xl leading-[120%] font-medium text-gray">
+                17,000
+              </div>
+            </div>
+            <div className="absolute top-[0px] left-[592px] w-[38px] h-[51px]">
+              <div className="absolute top-[0px] left-[4px] leading-[120%] font-medium">
+                APR
+              </div>
+              <div className="absolute top-[27px] left-[0px] text-xl leading-[120%] font-medium text-gray">
+                23%
+              </div>
+            </div>
+            <div className="absolute top-[80px] left-[277px] w-[165px] h-[51px]">
+              <div className="absolute top-[0px] left-[0px] leading-[120%] font-medium">
+                Total Rewards Claimed
+              </div>
+              <div className="absolute top-[27px] left-[36px] text-xl leading-[120%] font-medium text-gray">
+                17,000
+              </div>
+            </div>
+            <div className="absolute top-[80px] left-[558px] w-[105px] h-[51px]">
+              <div className="absolute top-[0px] left-[0px] leading-[120%] font-medium">
+                Staking Period
+              </div>
+              <div className="absolute top-[27px] left-[16px] text-xl leading-[120%] font-medium text-gray">
+                30 Days
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="absolute top-[702px] left-[163px] rounded-lg bg-gainsboro-100 w-[1120px] h-[620px] overflow-hidden">
+        <div className="absolute top-[52px] left-[calc(50%_-_399px)] w-[799px] h-[515px]">
+          <div className="absolute top-[0px] left-[0px] rounded bg-whitesmoke-300 box-border w-[382px] h-[515px] overflow-hidden border-[0.5px] border-solid border-black">
+            <div className="absolute top-[204px] left-[36px] w-[318px] h-[77px] hidden text-mini">
+              <div className="absolute top-[25px] left-[0px] rounded bg-whitesmoke-100 w-[318px] h-[52px] overflow-hidden">
+                <div className="absolute top-[calc(50%_-_17.5px)] left-[206px] rounded bg-gainsboro-200 w-[100px] flex flex-row items-center justify-start py-2 px-4 box-border gap-[34px]">
+                  <div className="relative leading-[120%] font-medium">ETH</div>
+                  <img
+                    className="relative rounded-12xs w-1.5 h-[6.5px]"
+                    alt=""
+                    src="/vector-1.svg"
+                  />
+                </div>
+                <div className="absolute top-[calc(50%_-_8px)] left-[12px] text-sm leading-[120%] font-light">
+                  100
+                </div>
+              </div>
+              <div className="absolute top-[calc(50%_-_38.5px)] left-[0px] text-sm leading-[120%] font-medium">
+                I will receive
+              </div>
+            </div>
+            <div className="absolute top-[calc(50%_+_68.5px)] left-[32px] w-[318px] h-[17px] hidden">
+              <div className="absolute top-[calc(50%_-_8.5px)] left-[0px] w-[318px] h-[17px]">
+                <div className="absolute top-[calc(50%_-_8.5px)] left-[0px] leading-[120%] font-medium">
+                  Transaction Fee
+                </div>
+                <div className="absolute top-[calc(50%_-_8.5px)] left-[261px] leading-[120%] font-light">
+                  0.1 Matic
+                </div>
+              </div>
+            </div>
+            <div className="absolute top-[calc(50%_+_109.5px)] left-[calc(50%_-_79px)] w-[163.79px] h-11 text-base text-gray">
+              <div className="absolute top-[calc(50%_-_22px)] left-[calc(50%_-_81.89px)] rounded-31xl bg-silver w-[163.79px] h-11 overflow-hidden">
+                <div className="absolute top-[calc(50%_-_12px)] left-[calc(50%_-_36.89px)] leading-[150%]">
+                  Stake NFT
+                </div>
+              </div>
+              <div className="absolute bottom-[4px] left-[calc(50%_+_13.56px)] rounded-31xl box-border w-[146.55px] h-11 overflow-hidden hidden text-lime border-[0.5px] border-solid border-lime">
+                <div className="absolute top-[calc(50%_-_12px)] left-[calc(50%_-_54.27px)] leading-[150%]">
+                  Claim Rewards
+                </div>
+              </div>
+            </div>
+            <img
+              className="absolute top-[154px] left-[172px] rounded-31xl w-[50px] h-[50px] overflow-hidden hidden"
+              alt=""
+              src="/frame-83.svg"
+            />
+            <div className="absolute top-[24px] left-[calc(50%_-_105px)] rounded-31xl bg-white w-[210px] h-11 overflow-hidden text-base text-gray">
+              <div className="absolute top-[calc(50%_-_18px)] left-[4px] rounded-31xl bg-silver w-[102px] h-9 overflow-hidden">
+                <b className="absolute top-[calc(50%_-_9px)] left-[calc(50%_-_20px)] leading-[120%]">
+                  Stake
+                </b>
+              </div>
+              <b className="absolute top-[calc(50%_-_9px)] left-[calc(50%_+_15px)] leading-[120%] text-darkslategray">
+                Borrow
+              </b>
+            </div>
+            <div className="absolute top-[92px] left-[32px] w-[318px] h-[233px]">
+              <div className="absolute top-[0px] left-[0px] w-[318px] h-[77px]">
+                <div className="absolute top-[0px] left-[0px] w-[318px] h-[77px]">
+                  <div className="absolute top-[25px] left-[0px] rounded bg-whitesmoke-100 w-[318px] h-[52px] overflow-hidden">
+                    <div className="absolute top-[6px] left-[12px] w-10 h-10">
+                      <div className="absolute top-[calc(50%_-_20px)] left-[calc(50%_-_20px)] rounded-t rounded-b-none bg-darkslategray w-10 h-10 overflow-hidden">
+                        <img
+                          className="absolute top-[0px] left-[0px] w-10 h-10 object-cover"
+                          alt=""
+                          src="/image-191@2x.png"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="absolute top-[calc(50%_-_38.5px)] left-[0px] leading-[120%] font-medium">
+                    NFT Details
+                  </div>
+                </div>
+                <div className="absolute top-[calc(50%_+_42.5px)] left-[213px] text-xs leading-[120%] font-light hidden">
+                  Available Matic: 500
+                </div>
+              </div>
+              <div className="absolute top-[175px] left-[0px] w-[318px] h-[58px] text-3xs">
+                <div className="absolute top-[0px] left-[0px] w-[318px] h-[58px]">
+                  <div className="absolute top-[42px] left-[0px] w-[318px] h-4">
+                    <div className="absolute top-[5px] left-[0px] rounded-31xl bg-gainsboro-200 w-[318px] h-1.5" />
+                    <div className="absolute top-[0px] left-[192px] rounded-[50%] bg-lime w-4 h-4" />
+                  </div>
+                  <div className="absolute top-[calc(50%_-_29px)] left-[0px] w-[276px] h-10">
+                    <div className="absolute top-[calc(50%_+_8px)] left-[54px] leading-[120%] font-light">
+                      7 Days
+                    </div>
+                    <div className="absolute top-[calc(50%_-_20px)] left-[0px] text-sm leading-[120%] font-medium">
+                      Staking Duration:
+                    </div>
+                    <div className="absolute top-[calc(50%_+_8px)] left-[112px] leading-[120%] font-light">
+                      14 Days
+                    </div>
+                    <div className="absolute top-[calc(50%_+_8px)] left-[176px] leading-[120%] font-light">
+                      21 Days
+                    </div>
+                    <div className="absolute top-[calc(50%_+_8px)] left-[240px] leading-[120%] font-light">
+                      28 Days
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="absolute top-[calc(50%_+_14.5px)] left-[0px] w-[318px] h-[17px]">
+                <div className="absolute top-[calc(50%_-_8.5px)] left-[0px] leading-[120%] font-medium">
+                  APR
+                </div>
+                <div className="absolute top-[calc(50%_-_8.5px)] left-[284px] leading-[120%] font-light">
+                  100%
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="absolute top-[0px] left-[417px] w-[382px] h-[515px] text-base">
+            <div className="absolute top-[0px] left-[0px] rounded bg-whitesmoke-300 box-border w-[382px] h-[515px] overflow-hidden border-[0.5px] border-solid border-black">
+              <div className="absolute top-[calc(50%_-_177.5px)] left-[32px] w-[318px] h-[139px] text-sm">
+                <div className="absolute top-[calc(50%_-_69.5px)] left-[0px] w-[318px] h-10">
+                  <div className="absolute top-[calc(50%_-_8px)] left-[0px] leading-[120%] font-medium">
+                    Mental Strength
+                  </div>
+                  <div className="absolute top-[calc(50%_-_20px)] left-[278px] w-10 h-10">
+                    <div className="absolute top-[calc(50%_-_20px)] left-[calc(50%_-_20px)] rounded-t rounded-b-none bg-darkslategray w-10 h-10 overflow-hidden">
+                      <img
+                        className="absolute top-[0px] left-[0px] w-10 h-10 object-cover"
+                        alt=""
+                        src="/image-192@2x.png"
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className="absolute top-[calc(50%_-_13.5px)] left-[0px] w-[318px] h-[17px]">
+                  <div className="absolute top-[calc(50%_-_8.5px)] left-[0px] leading-[120%] font-medium">
+                    APR
+                  </div>
+                  <div className="absolute top-[calc(50%_-_8.5px)] left-[299px] leading-[120%] font-light text-right">
+                    5%
+                  </div>
+                </div>
+                <div className="absolute top-[calc(50%_+_19.5px)] left-[0px] w-[318px] h-[17px]">
+                  <div className="absolute top-[calc(50%_-_8.5px)] left-[0px] leading-[120%] font-medium">
+                    Staking Duration
+                  </div>
+                  <div className="absolute top-[calc(50%_-_8.5px)] left-[268px] leading-[120%] font-light text-right">
+                    21 Days
+                  </div>
+                </div>
+                <div className="absolute top-[calc(50%_+_52.5px)] left-[0px] w-[318px] h-[17px]">
+                  <div className="absolute top-[calc(50%_-_8.5px)] left-[0px] leading-[120%] font-medium">
+                    Transaction Fee
+                  </div>
+                  <div className="absolute top-[calc(50%_-_8.5px)] left-[253px] leading-[120%] font-light text-right">
+                    0.01 Matic
+                  </div>
+                </div>
+              </div>
+              <div className="absolute top-[24px] left-[calc(50%_-_105px)] rounded-31xl bg-white w-[210px] h-11 overflow-hidden text-lime">
+                <div className="absolute top-[calc(50%_-_18px)] left-[4px] rounded-31xl bg-gray w-[102px] h-9 overflow-hidden">
+                  <b className="absolute top-[calc(50%_-_9px)] left-[calc(50%_-_35px)] leading-[120%]">
+                    Summary
+                  </b>
+                </div>
+                <b className="absolute top-[calc(50%_-_9px)] left-[calc(50%_+_22px)] leading-[120%] text-darkslategray">
+                  History
+                </b>
+              </div>
+              <div className="absolute bottom-[97px] left-[calc(50%_-_158px)] w-[318px] h-11 text-gray">
+                <div className="absolute bottom-[0px] left-[calc(50%_-_159px)] w-[318px] h-11">
+                  <div className="absolute bottom-[0px] left-[calc(50%_-_159px)] rounded-31xl bg-lime w-[207px] h-11 overflow-hidden">
+                    <div className="absolute top-[calc(50%_-_12px)] left-[calc(50%_-_58.5px)] leading-[150%]">
+                      Approve Staking
+                    </div>
+                  </div>
+                  <div className="absolute bottom-[0px] left-[calc(50%_-_159px)] rounded-31xl bg-lime w-[207px] h-11 overflow-hidden cursor-pointer">
+                    <div className="absolute top-[calc(50%_-_12px)] left-[calc(50%_-_58.5px)] leading-[150%]">
+                      Approve Staking
+                    </div>
+                  </div>
+                  <div className="absolute bottom-[0px] left-[calc(50%_+_58px)] rounded-31xl box-border w-[101px] h-11 overflow-hidden text-darkslategray border-[0.5px] border-solid border-darkslategray">
+                    <div className="absolute top-[calc(50%_-_12px)] left-[calc(50%_-_24.5px)] leading-[150%]">
+                      Cancel
+                    </div>
+                  </div>
+                  <div className="absolute bottom-[0px] left-[calc(50%_+_58px)] rounded-31xl box-border w-[101px] h-11 overflow-hidden cursor-pointer text-darkslategray border-[0.5px] border-solid border-darkslategray">
+                    <div className="absolute top-[calc(50%_-_12px)] left-[calc(50%_-_24.5px)] leading-[150%]">
+                      Cancel
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="absolute top-[71px] left-[42px] bg-gainsboro-200 w-9 h-[37px]" />
+      </div>
+      <div className="absolute top-[157px] left-[163px] flex flex-row items-center justify-start gap-[8px]">
+        <div className="relative leading-[120%] font-medium">Home</div>
+        <img className="relative w-3.5 h-3.5" alt="" src="/arrowleft.svg" />
+        <div className="relative leading-[120%] font-medium">
+          Liquidity Pool
+        </div>
+        <img className="relative w-3.5 h-3.5" alt="" src="/arrowleft1.svg" />
+        <div className="relative leading-[120%] font-medium">Stake NFT</div>
+      </div>
+    </div>
+  );
+}
+
+export default page;
