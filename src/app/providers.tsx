f1ace04f91e25060e12d9a76ae151ea860e06b74
anurag <anuragpathakoffl@gmail.com>
2023-12-06 03:23:20 +0530

    added chatbot UI

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,40 +1,57 @@
-'use client'
-import { WagmiConfig, createConfig, configureChains, mainnet } from 'wagmi'
+"use client";
+import { WagmiConfig, createConfig, configureChains, mainnet } from "wagmi";
 
-import { alchemyProvider } from 'wagmi/providers/alchemy'
-import { publicProvider } from 'wagmi/providers/public'
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
 
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
-import {polygonMumbai} from "viem/chains";
-import {NextUIProvider} from "@nextui-org/system";
-import { QueryClientProvider } from '@tanstack/react-query'
-import {QueryClient} from "@tanstack/query-core";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { polygonMumbai } from "viem/chains";
+import { NextUIProvider } from "@nextui-org/system";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient } from "@tanstack/query-core";
 
-
+import { MessagesProvider } from "@/context/messages";
+import { FC, ReactNode } from "react";
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-    [polygonMumbai],
-    [alchemyProvider({ apiKey: '0Z8bgqW0ESlNsj7Zl7GoLtzL9yVhwqmh' }), publicProvider()],
-)//remove hardcoded api key
+  [polygonMumbai],
+  [
+    alchemyProvider({ apiKey: "0Z8bgqW0ESlNsj7Zl7GoLtzL9yVhwqmh" }),
+    publicProvider(),
+  ]
+); //remove hardcoded api key
 
 const config = createConfig({
-    autoConnect: true,
-    connectors: [
-        new MetaMaskConnector({ chains })
-    ],
-    publicClient,
-    webSocketPublicClient
-})
-const queryClient = new QueryClient()
+  autoConnect: true,
+  connectors: [new MetaMaskConnector({ chains })],
+  publicClient,
+  webSocketPublicClient,
+});
+
+const queryClient = new QueryClient();
 
 export function Providers({ children }) {
-    return (
-        <WagmiConfig config={config}>
-            <QueryClientProvider client={queryClient}>
-                <NextUIProvider>
-                    {children}
-                </NextUIProvider>
-            </QueryClientProvider>
-        </WagmiConfig>
-    );
+  return (
+    <WagmiConfig config={config}>
+      <QueryClientProvider client={queryClient}>
+        <NextUIProvider>{children}</NextUIProvider>
+      </QueryClientProvider>
+    </WagmiConfig>
+  );
 }
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: FC<LayoutProps> = ({ children }) => {
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MessagesProvider>{children}</MessagesProvider>
+    </QueryClientProvider>
+  );
+};
+
+export default Layout;
