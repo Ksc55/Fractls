c04cf0b5ddcd3901f07dab3d90b79629003b7182
MathisZerbib <mathis.zerbib@gmail.com>
2023-12-09 00:02:14 +0100

    Added right config for AI fraud detector

--- AI/similarity.py
+++ AI/similarity.py
@@ -30,6 +30,11 @@ def load_image(image_path):
     """
 
     input_image = Image.open(image_path)
+    
+    # Convert image to RGB if it has an alpha channel
+    if input_image.mode == 'RGBA':
+        input_image = input_image.convert('RGB')
+
     resized_image = input_image.resize((224, 224))
 
     return resized_image
@@ -56,7 +61,6 @@ def get_similarity_score(first_image : str, second_image : str):
         return embedding of the image
 
     """
-
     first_image = load_image(first_image)
     second_image = load_image(second_image)
 
@@ -64,7 +68,7 @@ def get_similarity_score(first_image : str, second_image : str):
     second_image_vector = get_image_embeddings(second_image)
 
     similarity_score = cosine_similarity(first_image_vector, second_image_vector).reshape(1,)
-
+    print(f"Similarity score: {similarity_score}")
     return similarity_score
 
 def show_image(image_path):
@@ -72,43 +76,3 @@ def show_image(image_path):
   imgplot = plt.imshow(image)
   plt.savefig('temp.png')  # Save the figure to a file instead of showing
   plt.close()
-
-# define the path of the images
-image1 = './content/1.jpg'
-image2 = './content/2.jpg'
-
-# use the show_image function to plot the images
-show_image(image1), show_image(image2)
-
-similarity_score = get_similarity_score(image1, image2)
-print(similarity_score)
-
-# # define the path of the images
-super = './content/super.jpg'
-
-show_image(image1), show_image(super)
-
-similarity_score = get_similarity_score(image1, super)
-
-print(similarity_score)
-
-# Delete the temporary image file
-os.remove('temp.png')
-
-# Workflow => 
-
-
-# TODO make a route in express to store the source image and the nft that have been already minted (Graqhql)
-
-# similarity page should be done 
-# on this page we need to show the fractionnal nft and in subsection we need to list all the similar nft
-# on a page similare nft should be listed 
-
-
-
-
-
-# 1. Load an image 
-# 2. Compare it to a list of image
-# 4.else return the one best matching (close to 0.8 => 0.999)
-
