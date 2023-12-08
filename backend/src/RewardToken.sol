20d71ee7b45d2ab3a39fd46bf1700c2fcda6400b
king lee <c-king.lee@hulu.com>
2023-12-08 12:09:13 -0600

    removing bc folder

--- backend/src/RewardToken.sol
+++
@@ -1,4 +0,0 @@
-//After the randon  number gotten 1 hours 
-// we perform a maths logic
-// 1-5
-//1 9 17 25 26 27 2 3 4 5 67
--- backend/src/interface/INFTMarketplace.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface INFTMarketplace {
-    function checkWinner(
-        uint256 _tokenId
-    ) external view returns (address _winnerAddr);
-}
