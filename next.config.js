1ab384e979d8c5430040408e07f93b4f3b224788
king lee <c-king.lee@hulu.com>
2023-12-09 08:16:44 -0600

    about page

--- next.config.js
+++ next.config.js
@@ -28,6 +28,10 @@ const nextConfig = {
       {
         protocol: 'https',
         hostname: 'gateway.ipfs.io'
+      },
+      {
+        protocol: 'https',
+        hostname: 'ipfs.nftstorage.link'
       }
     ]
   }
