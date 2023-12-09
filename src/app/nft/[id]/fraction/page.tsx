d25348d9b0960aadcdfb77253844bd72610cd70c
king lee <c-king.lee@hulu.com>
2023-12-09 14:51:05 -0600

    random nfts

--- src/app/nft/[id]/fraction/page.tsx
+++ src/app/nft/[id]/fraction/page.tsx
@@ -1,12 +1,41 @@
 'use client'
 import Image from "next/image";
-import {useContractRead} from "wagmi";
+import {useContractRead, useContractReads, useContractWrite} from "wagmi";
 import NFTMarketplace from "@/NFTMarketplace.json";
 import {useQuery} from "@tanstack/react-query";
 import {fetchIPFS} from "@/app/marketplace/page";
 
+
 export default function Page(props) {
 
+    const {data: NFTList = [], isLoading} = useContractRead({
+        address: process.env.NEXT_PUBLIC_CONTRACT,
+        abi: NFTMarketplace.abi,
+        functionName: 'getAllNFTs',
+    })
+
+    const {data: NFTFractions = [],} = useContractReads({
+        contracts: NFTList.flatMap(nft =>
+            nft.partIds.map(id => {
+                    return {
+                        address: process.env.NEXT_PUBLIC_CONTRACT,
+                        abi: NFTMarketplace.abi,
+                        functionName: 'getPartURI',
+                        args: [nft.tokenId, id],
+                    }
+                }
+            )
+        ),
+    })
+    const randomNFTS = getRandomElementsFromArray(NFTFractions, 5)
+    const {data: transactionData, isLoading: loadingTransaction, isSuccess, write} = useContractWrite({
+        address: process.env.NEXT_PUBLIC_CONTRACT,
+        abi: NFTMarketplace.abi,
+        functionName: 'createNFT'
+    })
+    const onBuy = async () => {
+        write({args: []})
+    }
     const {data: NFTData, isError} = useContractRead({
             address: process.env.NEXT_PUBLIC_CONTRACT,
             abi: NFTMarketplace.abi,
@@ -21,11 +50,12 @@ export default function Page(props) {
             enabled: !!NFTData,
         }
     )
-    console.log('metadata', metadata)
+    console.log('NFTFractions', NFTFractions)
 
     if (metadata) {
+        const price = NFTData.price === undefined ? 0: NFTData.price.toString();
+
         const url = `https://gateway.ipfs.io/ipfs/${metadata.image}${metadata.path}.png`
-        console.log('url', url)
         return (<>
             <div className="flex flex-col  h-screen">
                 <section className={'w-100  bg-[#d9d9d9]  h-3/6 flex justify-center items center'}>
@@ -43,32 +73,25 @@ export default function Page(props) {
                         <div
                             className=" w-[475px] h-16 rounded-[0.875rem] border border-black bg-white mt-4 flex justify-between items-center px-2">
                             <div className={'flex flex-col'}>
-                                <div className="text-black/[.70] font-['Roboto'] text-[.9375rem] leading-[normal]">Last
-                                    Sold
+
+                                <div className="text-black font-['Roboto'] text-2xl leading-[normal]">{price} ETH
                                 </div>
-                                <div className="text-black font-['Roboto'] text-2xl leading-[normal]">0.55 ETH</div>
                             </div>
-                            <button className="flex-shrink-0 w-[7.1875rem] h-10 rounded-full bg-customGreen-50">
+                            <button className="flex-shrink-0 w-[7.1875rem] h-10 rounded-full bg-customGreen-50"
+                                    onClick={onBuy}>
                                 Buy
                             </button>
                         </div>
                     </div>
                 </section>
-                <div className={'flex justify-between items-center mt-20'}>
-                    <Image alt={'tmp'} src={'/back-arrow.svg'} width={80} height={80} className={'p-4'}/>
+                <div className={'flex justify-center items-center my-20'}>
                     <div
                         className="text-[#121212] text-center font-['Roboto'] text-[2rem] font-semibold leading-[150%]">Similar
                         F-NFTs
                     </div>
-                    <Image alt={'tmp'} src={'/next-arrow.svg'} width={80} height={80} className={'p-4'}/>
                 </div>
                 <div className={'flex justify-between items-center'}>
-                    <Image alt={'tmp'} src={'/image-19.png'} width={250} height={250}/>
-                    <Image alt={'tmp'} src={'/image-19.png'} width={250} height={250}/>
-                    <Image alt={'tmp'} src={'/image-19.png'} width={250} height={250}/>
-                    <Image alt={'tmp'} src={'/image-19.png'} width={250} height={250}/>
-                    <Image alt={'tmp'} src={'/image-19.png'} width={250} height={250}/>
-
+                    {randomNFTS.map(_nft => <RandomNFTCard nft={_nft.result}/>)}
                 </div>
             </div>
         </>)
@@ -76,4 +99,34 @@ export default function Page(props) {
 }
 
 
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
 
+function getRandomElementsFromArray(array, count) {
+    const shuffledArray = shuffleArray([...array]); // Create a copy to avoid modifying the original array
+    return shuffledArray.slice(0, count);
+}
+
+
+const RandomNFTCard = ({nft}) => {
+    const {data: metadata} = useQuery(
+        {
+            queryKey: ['metadata', nft.id.toString()],
+            queryFn: () => fetchIPFS(nft.url),
+        }
+    )
+    if (metadata) {
+        const url = `https://gateway.ipfs.io/ipfs/${metadata.image}${metadata.path}.png`
+        return (
+            <div className={'flex flex-col justify-center items-center border-2 border-stale-950'}>
+                <Image src={url} width={250} height={250} className={'p-4'} alt={'img'}/>
+            </div>
+        )
+    }
+
+}
