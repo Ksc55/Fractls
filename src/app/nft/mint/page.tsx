ff77a7b2d111bc2aac4543236ee6dc9409eb9bad
MathisZerbib <mathis.zerbib@gmail.com>
2023-12-09 23:24:44 +0100

    Add poc example of antifraud AI
    When The service is called with a image that return a 504 then need to be redeployed ...

--- src/app/nft/mint/page.tsx
+++ src/app/nft/mint/page.tsx
@@ -1,111 +1,159 @@
-'use client'
+"use client";
 import ImageUploader from "@/components/ImageUploader";
 import StyledInput from "@/components/InputField";
-import FractionatedNTFFields, {Shard} from "@/components/FractionatedNTFFields";
-import {useState} from "react";
-import {useAccount, useContractRead, useContractWrite, usePrepareContractWrite} from "wagmi";
-import {Modal, ModalBody, ModalContent, ModalHeader} from "@nextui-org/modal";
-import {useDisclosure} from "@nextui-org/use-disclosure";
-import {useMutation} from "@tanstack/react-query";
+import FractionatedNTFFields, {
+  Shard,
+} from "@/components/FractionatedNTFFields";
+import { useState } from "react";
+import {
+  useAccount,
+  useContractRead,
+  useContractWrite,
+  usePrepareContractWrite,
+} from "wagmi";
+import { Modal, ModalBody, ModalContent, ModalHeader } from "@nextui-org/modal";
+import { useDisclosure } from "@nextui-org/use-disclosure";
+import { useMutation } from "@tanstack/react-query";
 import NFTMarketplace from "../../../NFTMarketplace.json";
+import { CircularProgress } from "@nextui-org/react";
 
 interface formValues {
-    name: string;
-    description: string;
-    shards: Shard[];
+  name: string;
+  description: string;
+  shards: Shard[];
 }
-const saveToIpfs = async ({image, name , description}) => {
-    const res = await fetch('/nft/upload', {
-        method: 'POST',
-        body: JSON.stringify({image, name, description}),
-    })
-    return await res.json()
-}
+const saveToIpfs = async ({ image, name, description }) => {
+  const res = await fetch("/nft/upload", {
+    method: "POST",
+    body: JSON.stringify({ image, name, description }),
+  });
+  return await res.json();
+};
 export default function Page() {
-    const { address } = useAccount()
-    const {mutateAsync, isLoading} = useMutation(saveToIpfs)
-    const {isOpen, onOpen, onOpenChange} = useDisclosure();
-    const [modal, setModal] = useState(false);
-    const { data, isError } = useContractRead({
-        address: process.env.NEXT_PUBLIC_CONTRACT,
-        abi: NFTMarketplace.abi,
-        functionName: 'getAllNFTs',
-    })
-    console.log(data)
-    const [selectedImage, setSelectedImage] = useState<string | null>(null);
-    const [form, setForm] = useState<formValues>({name: '', description: '', shards: []});
-    const {name, description} = form;
-    const inValidFrom = !name || !description;
-    const { data: transactionData, isLoading: loadingTransaction, isSuccess, write } = useContractWrite({
-        address: process.env.NEXT_PUBLIC_CONTRACT,
-        abi: NFTMarketplace.abi,
-        functionName: 'createNFT'
-    })
-    const onChangeField = (e: any) => {
-        setForm({...form, [e.target.name]: e.target.value});
-    }
-    const onChangeShards = (shards: Shard[]) => {
-        setForm({...form, shards});
-    }
-    const mintTokens = async () => {
-        setModal(true)
-        const response = await mutateAsync({image: selectedImage, name: form.name, description: form.description})
-        write({args: [response.tokenId, response.tokenURIs, response._ids]})
-    };
-    if (address === undefined) {
-        return (<div className={'flex justify-center items-center h-screen'}>
-            <h1 className={'text-3xl font-bold'}>Welcome please connect wallet</h1>
-        </div>)
-    }
+  const { address } = useAccount();
+  const { mutateAsync, isLoading } = useMutation(saveToIpfs);
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [modal, setModal] = useState(false);
+  const { data, isError } = useContractRead({
+    address: process.env.NEXT_PUBLIC_CONTRACT,
+    abi: NFTMarketplace.abi,
+    functionName: "getAllNFTs",
+  });
+  console.log(data);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [form, setForm] = useState<formValues>({
+    name: "",
+    description: "",
+    shards: [],
+  });
+  const { name, description } = form;
+  const inValidFrom = !name || !description;
+  const {
+    data: transactionData,
+    isLoading: loadingTransaction,
+    isSuccess,
+    write,
+  } = useContractWrite({
+    address: process.env.NEXT_PUBLIC_CONTRACT,
+    abi: NFTMarketplace.abi,
+    functionName: "createNFT",
+  });
+  const onChangeField = (e: any) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const onChangeShards = (shards: Shard[]) => {
+    setForm({ ...form, shards });
+  };
+  const mintTokens = async () => {
+    setModal(true);
+    const response = await mutateAsync({
+      image: selectedImage,
+      name: form.name,
+      description: form.description,
+    });
+    write({ args: [response.tokenId, response.tokenURIs, response._ids] });
+  };
+  if (address === undefined) {
     return (
+      <div className={"flex justify-center items-center h-screen"}>
+        <h1 className={"text-3xl font-bold"}>Welcome please connect wallet</h1>
+      </div>
+    );
+  }
+  return (
+    <>
+      <Modal
+        {...{ isOpen: modal, onOpen, onOpenChange }}
+        backdrop={"blur"}
+        hideCloseButton
+      >
+        <ModalContent>
+          <ModalContent>
+            <ModalHeader>Minting NFT</ModalHeader>
+            <ModalBody>
+              {isLoading && (
+                <div className="flex items-center justify-center flex-col my-5">
+                  <p className="text-xl font-semibold mb-4">Storing your NFT</p>
+                  <CircularProgress className="text-center mb-4" />
+                </div>
+              )}
+              {loadingTransaction && (
+                <p className="text-center text-2xl font-bold my-10">
+                  Check your Wallet
+                </p>
+              )}
+              {isSuccess && (
+                <div className="flex justify-center mt-10">
+                  <a
+                    href="/marketplace"
+                    className={`bg-customGreen-50 rounded-full px-4 h-10 font-light w-full text-center `}
+                  >
+                    Check on Marketplace
+                  </a>
+                </div>
+              )}
+            </ModalBody>
+          </ModalContent>
+        </ModalContent>
+      </Modal>
+      <div className="flex justify-center items-center bg-customGray-50 h-96 mt-10">
+        <ImageUploader {...{ selectedImage, setSelectedImage }} />
+      </div>
+      <div className="flex justify-between mt-10 gap-4">
+        <StyledInput
+          placeholder="Name of the NFT"
+          value={name}
+          onChange={onChangeField}
+          name={"name"}
+        />
+        <StyledInput
+          placeholder="Description"
+          value={description}
+          onChange={onChangeField}
+          name={"description"}
+        />
+      </div>
+      {selectedImage && (
         <>
-            <Modal {...{isOpen: modal, onOpen, onOpenChange}} backdrop={'blur'} hideCloseButton>
-                <ModalContent>
-                    <ModalContent>
-                        <ModalHeader>Minting NFT</ModalHeader>
-                        <ModalBody>
-                            {
-                                isLoading && <p className="text-center text-2xl font-bold my-10">Storing your NFT</p>
-                            }
-                            {
-                                loadingTransaction && <p className="text-center text-2xl font-bold my-10">Check your Wallet</p>
-                            }
-                            {
-                                isSuccess && <div className="flex justify-center mt-10">
-                                    <a
-                                        href="/marketplace"
-                                        className={`bg-customGreen-50 rounded-full px-4 h-10 font-light w-full text-center `}
-                                    >
-                                        Check on Marketplace
-                                    </a>
-                              </div>
-                            }
-                        </ModalBody>
-                    </ModalContent>
-                </ModalContent>
-            </Modal>
-            <div className="flex justify-center items-center bg-customGray-50 h-96 mt-10">
-                <ImageUploader {...{selectedImage, setSelectedImage}}/>
-            </div>
-            <div className="flex justify-between mt-10 gap-4">
-                <StyledInput placeholder="Name of the NFT" value={name} onChange={onChangeField} name={'name'}/>
-                <StyledInput placeholder="Description" value={description} onChange={onChangeField} name={'description'}/>
-            </div>
-            { selectedImage && <>
-              <p className="text-center text-2xl font-bold my-10">Fractionalized NFTs</p>
-                <FractionatedNTFFields image={selectedImage} setValue={onChangeShards}/>
-              <div className={'flex justify-center mt-10 '}>
-                <button
-                  onClick={mintTokens}
-                  disabled={inValidFrom}
-                  className={`bg-customGreen-50 rounded-full px-4 h-10 font-light w-1/6 
-                  ${inValidFrom ? 'opacity-50 cursor-not-allowed' : ''}`}
-                >
-                  MINT
-                </button>
-              </div>
-              </>
-            }
+          <p className="text-center text-2xl font-bold my-10">
+            Fractionalized NFTs
+          </p>
+          <FractionatedNTFFields
+            image={selectedImage}
+            setValue={onChangeShards}
+          />
+          <div className={"flex justify-center mt-10 "}>
+            <button
+              onClick={mintTokens}
+              disabled={inValidFrom}
+              className={`bg-customGreen-50 rounded-full px-4 h-10 font-light w-1/6 
+                  ${inValidFrom ? "opacity-50 cursor-not-allowed" : ""}`}
+            >
+              MINT
+            </button>
+          </div>
         </>
-    )
+      )}
+    </>
+  );
 }
--- src/app/nft/upload/route.ts
+++ src/app/nft/upload/route.ts
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 import { createCanvas, loadImage } from "canvas";
 import {NextResponse} from "next/server";
 import { v4 as uuidv4 } from 'uuid';
+import {useAccount, useContractRead, useContractWrite, usePrepareContractWrite} from "wagmi";
+import NFTMarketplace from "../../../NFTMarketplace.json";
+
 
 dotenv.config();
 
@@ -78,7 +81,7 @@ async function storePuzzleAsset(originalNFT, name ,description) {
         const originalImageMetadata = {
             name,
             description,
-            image: new File([originalNFT], "PuzzleNFT.png", { type: "image/png" }),
+            image: new File([canvas.toBuffer("image/png")], `original.png`, { type: "image/png" }),
             properties: {
                 pieces: puzzlePiecesMetadata.map((pieceMetadata) => pieceMetadata.url),
             },
@@ -86,18 +89,147 @@ async function storePuzzleAsset(originalNFT, name ,description) {
 
         // Store the metadata and image for the original image on NFT.Storage
         const originalImage = await client.store(originalImageMetadata);
-        console.log("Metadata stored on Filecoin and IPFS for original image:");
-        console.log(originalImage.url);
+        console.log("Metadata stored on Filecoin and IPFS for original image:" + originalImage.url);
+        
+
+
+        // HERE CHECK ANTI FRAUD
+        // try {
+        //     await checkFraud(originalImage)
+        // }
+        // catch (e) {
+        //     console.log(e)
+        // }
+
+
+
         return puzzlePiecesMetadata.map((metadata, index) => ({
             url: metadata.url,
             id: uuidv4().split('-').map(part => parseInt(part, 16)).join(''),
         }));
+
+        
+
+        
+
+
+
+        
     } catch (error) {
         // Handle any errors that may occur during the process
         console.error(error);
         throw error;
     }
 }
+
+
+
+const checkFraud = async (originalImage) => {
+    const responseIpfs = await getImageUrlFromIpfs(originalImage.url);
+            await checkAntiFraud(responseIpfs);
+  };
+
+
+async function getImageUrlFromIpfs(ipfsUrl: string) {
+    const ipfsEndpoint = "https://gateway.pinata.cloud";
+    ipfsUrl = ipfsUrl.replace('ipfs://', '');
+    const fullUrl = `${ipfsEndpoint}/ipfs/${ipfsUrl}`;
+
+    try {
+        const response = await fetch(fullUrl);
+
+        if (!response.ok) {
+            throw new Error(`Failed to fetch IPFS image: ${response.status} ${response.statusText}`);
+        }
+
+        const responseData = await response.text();
+
+        const data = JSON.parse(responseData);
+        return data.image;
+    } catch (error) {
+        console.error("Error fetching IPFS image:", error);
+        try {
+            const response = await fetch("https://ipfs.io/ipfs/" + ipfsUrl);
+
+            if (!response.ok) {
+                throw new Error(`Failed to fetch IPFS image: ${response.status} ${response.statusText}`);
+            }
+
+            const responseData = await response.text();
+
+            const data = JSON.parse(responseData);
+            return data.image;
+        }
+        catch (e) {
+            console.error("Error fetching IPFS image:", e);
+        }
+        
+    }
+}
+
+
+export async function checkAntiFraud(image: string) {
+    try {
+        const antifraud_endpoint = process.env.ANTI_FRAUD_URL;
+        image = image.replace('ipfs://', '');
+        console.log('image:', image);
+
+        const response = await fetch(antifraud_endpoint + '/run-similarity', {
+            method: 'POST',
+            body: JSON.stringify([
+                image,
+                "bafybeihwetgkyyye2reyev7i7dzf37w3nd52pxwwzzqh56ew6ulktamtya/piece9.png",
+                "bafybeifrlbawyaaswpwzlk3zx2euvppybnag3d5b3sa6z6nijjm5mrq5dy/piece2.png"
+            ]),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log("ANTIFRAUD Result:", data);
+        } else {
+            console.error("ANTIFRAUD Error:", response.status, response.statusText);
+        }
+    } catch (error) {
+        console.error("ANTIFRAUD Error:", error);
+    }
+}
+
+
+// import NFTMarketplace from "../../../NFTMarketplace.json";
+
+// interface UseTriggerGetAllNFTs {
+//     data: ReturnType<typeof useContractRead>['data'];
+//     isError: ReturnType<typeof useContractRead>['isError'];
+//     triggerGetAllNFTs: () => Promise<any>; // Adjust 'any' to the actual type returned by getAllNFTs
+// }
+
+// // Custom hook to trigger the getAllNFTs function
+// export function useTriggerGetAllNFTs(): UseTriggerGetAllNFTs {
+//     const triggerGetAllNFTs = async () => {
+//         try {
+//             // Call the getAllNFTs function
+//             const result = await useContractRead({
+//                 address: `0x${process.env.NEXT_PUBLIC_CONTRACT}`,
+//                 abi: NFTMarketplace.abi,
+//                 functionName: "getAllNFTs",
+//             });
+
+//             // Handle the result if needed
+//             console.log("Result of getAllNFTs:", result);
+//             return result;
+//         } catch (error) {
+//             console.error("Error triggering getAllNFTs:", error);
+//             throw error;
+//         }
+//     };
+
+//     return { data, isError, triggerGetAllNFTs };
+// }
+
+
 
 
 
